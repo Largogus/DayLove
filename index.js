0ead19cde3d358c3df0fc33ffd6edcdf06342b11
Largen <lvovalex09@gmail.com>
2025-02-05 22:34:22 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     const background = document.querySelector('.background');
+//     const images = [
+//         'image/back1',
+//         'image/back2'
+//     ]
+
+//     let currentBackground = 0;
+
+//     setInterval(() => {
+//         currentBackground = (currentBackground + 1) % images.length;
+//         background.style.backgroundImage = `url(${images[currentBackground]})`;
+//         console.log(images[currentBackground]);
+//     }, 2000);
+// });
